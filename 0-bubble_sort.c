26559ff304bf766ad2b6230fce5465f65d729871
Ifaol <ifaolhunde@gmail.com>
2024-01-22 05:42:07 -0800

    bubble sort function updated for efficiency

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,18 +1,5 @@
 #include "sort.h"
 /**
- *swap - a swap two values function
- *@x: int pointer input
- *@y: int pointer input
- *
- *Return: void
- */
-void swap(int *x, int *y)
-{
-int temp = *x;
-*x = *y;
-*y = temp;
-}
-/**
  *bubble_sort - a bubble sort function
  *@array: array input to be sorted
  *@size: size of the array
@@ -22,7 +9,12 @@ int temp = *x;
 void bubble_sort(int *array, size_t size)
 {
 size_t i, j;
+int temp;
 bool is_swapped;
+if (array == NULL || size < 2)
+{
+return;
+}
 for (i = 0 ; i < size - 1 ; i++)
 {
 is_swapped = false;
@@ -30,7 +22,9 @@ for (j = 0 ; j < size - i - 1 ; j++)
 {
 if (array[j] > array[j + 1])
 {
-swap(&array[j], &array[j + 1]);
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
 print_array(array, size);
 is_swapped = true;
 }
